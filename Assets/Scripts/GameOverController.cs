327c3a419ad331a779fe6b72d6e51f0c954aebac
Sudhamshu chaudary <chaudharysudhansu1@gmail.com>
2026-05-15 18:21:21 +0545

    fix: minor bugs

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -11,5 +11,11 @@ public class GameOverController : MonoBehaviour
             // Reload the current scene to restart the game
             SceneManager.LoadScene("GameScene");
         }
+        // Optionally, you can also check for the "Enter" key on the numeric keypad
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            // Reload the current scene to restart the game
+            SceneManager.LoadScene("GameScene");
+        }
     }
 }
